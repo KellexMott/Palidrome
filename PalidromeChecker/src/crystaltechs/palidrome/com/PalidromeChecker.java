942735a3b0d2479627fd7d7b789b1a315d3f2750
KelvinMott <kelvinmott@gmail.com>
2017-04-04 16:26:04 +0300

    Package renamed

---
+++ PalidromeChecker/src/crystaltechs/palidrome/com/PalidromeChecker.java
@@ -0,0 +1,15 @@
+package crystaltechs.palidrome.com;
+
+public class PalidromeChecker {
+
+	public boolean isPalidrome(String string) {
+		
+		StringBuffer reverseString = new StringBuffer(string);
+		reverseString.reverse();
+		
+		if (string.equalsIgnoreCase(reverseString.toString()))
+			return true;
+		return false;
+	}
+
+}
---
+++ PalidromeChecker/src/crystaltechs/palidrome/com/PalidromeCheckerTest.java
@@ -0,0 +1,23 @@
+package crystaltechs.palidrome.com;
+import junit.framework.TestCase;
+
+public class PalidromeCheckerTest extends TestCase {
+		
+	public void testIsObjectNull(){
+		PalidromeChecker testObject = new PalidromeChecker();
+		assertNotNull(testObject);
+	}
+	
+	public void testIsStringPalidrome() throws Exception
+	{
+		PalidromeChecker testObject = new PalidromeChecker();
+		assertTrue(testObject.isPalidrome("MOM"));
+	}
+
+	public void testIsNotStringPalidrome() throws Exception
+	{
+		PalidromeChecker testObject = new PalidromeChecker();
+		assertFalse(testObject.isPalidrome("DAD"));
+	}
+	
+}
